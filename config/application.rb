695cebc4acb0318498cf33279ce7f61d3d212cae
Tomwey <tomwey@163.com>
2013-04-08 23:07:54 +0800

    do not generator standalone css, js, and helper file.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,14 @@ end
 
 module LawWenda
   class Application < Rails::Application
+    
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+      g.helper_specs false
+      g.view_specs false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
