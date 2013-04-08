ca403eb0911cb2fb5653ebde9a3d01f5c70a8d4b
Tomwey <tomwey@163.com>
2013-04-08 22:54:37 +0800

    init project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LawWenda::Application.config.secret_token = '8bbed9c6a5c2b2283ee12cbbf4288f95255d026c8d220f56eea6efc36e7bdb0a17ca9c91ccb0b4fb86c10ef1a7397d4957f901f24358746f9124252d9b26c38a'
