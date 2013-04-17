1ef0cf7528b76c4d46d77c44a7200e6385439b68
Tomwey <tomwey@163.com>
2013-04-17 23:50:20 +0800

    xxx

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 LawWenda::Application.routes.draw do
   devise_for :users
 
-  authenticated :user do
-    root to: 'home#index'
-  end
-  
-  as :user do
-    root to: 'devise/registrations#new'
-  end
+  root to: 'home#index'
+  # authenticated :user do
+  #   root to: 'home#index'
+  # end
+  # 
+  # as :user do
+  #   root to: 'devise/registrations#new'
+  # end
   
 end
