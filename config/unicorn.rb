5c5745e34561199ab315e75dd88e19adb6cea326
Tomwey <tomwey@163.com>
2013-06-04 00:23:53 +0800

    xx

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 root = "/home/deployer/apps/law-wenda/current"
 working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
+pid "#{root}/tmp/pids/unicorn_law-wenda.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.law-wenda.sock"
+listen "/tmp/unicorn.law-wenda2.sock"
 worker_processes 2
 timeout 30
